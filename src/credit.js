0fc8f3e67ae753b412237f7ce5ce4c60fecd3fbd
Angelica <Angelica137@users.noreply.github.com>
2020-06-14 18:40:34 +0100

    refactor, delete debit

--- src/credit.js
+++ src/credit.js
@@ -1,4 +1,4 @@
-import moment from 'moment';
+import moment from "moment";
 
 export class TransactionCredit {
   constructor(amount, balance = 0) {
@@ -13,17 +13,16 @@ export class TransactionCredit {
       date: this._formatDate(),
       credit: this.credit,
       debit: this.debit,
-      balance: this.balance
-      }
+      balance: this.balance,
+    };
   }
 
   _formatDate() {
     return moment(this.date).format("DD/MM/YYYY ");
   }
 
-//add method to display trasanciton as per table requirements
-//this is a string
-//this class is reponsible for formatting it self
-//create method to send transaction to transaction history
-
+  //add method to display trasnanciton as per table requirements
+  //this is a string
+  //this class is reponsible for formatting it self
+  //create method to send transaction to transaction history
 }
